a2b0fc3d333caf5c52c06e8b1c040aa0a0819b1e
Berwanger29 <viniciusberwangers@gmail.com>
2023-06-26 16:41:34 -0400

    menu hamburguer working

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const hamburguer = document.querySelector(".hamburguer")
-const navMenu = document.querySelector('.nav-menu')
-
-hamburguer.addEventListener("click", ()=>{
-    hamburguer.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
-
-document.querySelectorAll(".nav-item").forEach(n=> n.addEventListener("click", ()=>{
-    hamburguer.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
-
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
 
 function submitForm(){
     alert('Action submited, thanks')
